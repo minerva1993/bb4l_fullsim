567c1829f2e7fe7a1b27cd3cc38175645b3162b3
Jiwon Park <jiwonpark1993@gmail.com>
2026-03-30 18:10:14 +0900

    knu

---
+++ gen/run_gridpack_knu.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+NEVENTS=$1
+JOBID=$2
+NFILE=$3
+GRIDPACK=$4
+
+GRIDPACK=`realpath ${GRIDPACK}`
+SEED1=`echo "526719+${2}+${3}" | bc`
+
+JOBNAME=$(basename "$GRIDPACK")
+JOBNAME="${JOBNAME%.*}"
+
+echo $SEED1
+
+#PNFS_HOME="/store/user/jipark/bb4l_tests"
+#PNFS_OUT=${PNFS_HOME}/job_${JOBNAME}_${JOBID}
+
+cd /u/user/jipark/bb4l_fullsim/gen
+
+OUTDIR=`realpath .`/out/job_${JOBNAME}_${JOBID}
+
+echo "Gridpack: ${GRIDPACK}"
+echo "Output directory: ${RUNDIR}"
+
+RUNDIR=`mktemp -d -p /tmp`
+echo "Run directory: ${RUNDIR}"
+#mkdir -p $RUNDIR
+cd $RUNDIR
+
+cat /etc/os-release
+
+export SCRAM_ARCH=slc7_amd64_gcc700
+
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+
+export X509_USER_PROXY=~/.globus/x509up
+
+mkdir -p $OUTDIR
+
+tar -xf ${GRIDPACK}
+
+./runcmsgrid.sh ${NEVENTS} ${SEED1} 1
+
+gzip cmsgrid_final.lhe
+mv cmsgrid_final.lhe.gz LHE_$NFILE.lhe.gz
+
+#echo "Generation done, copying to PNFS at ${PNFS_OUT}"
+#
+#xrdfs root://dcache-cms-xrootd.desy.de mkdir ${PNFS_OUT} 
+#xrdcp LHE_$NFILE.lhe.gz root://dcache-cms-xrootd.desy.de/${PNFS_OUT}
+#
+#if [ $? -eq 0 ]; then
+#    echo "Copied file successfully"
+#else
+#    echo "File copy failed, copying to NFS..."
+#    cp LHE_$NFILE.lhe.gz $OUTDIR
+#fi
+
+cp LHE_$NFILE.lhe.gz $OUTDIR
+
+cd ..
+rm -rf $RUNDIR
